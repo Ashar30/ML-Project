7e81addaf5e9f57935ace07a51b982d10706e3c6
Ashar30 <asharali0504@gmail.com>
2026-07-01 13:58:52 +0530

    Data Ingestion

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from src.mlproject.logger import logging
 from src.mlproject.exceptions import CustomException
 import sys
+from src.mlproject.components.data_ingestion import DataIngestion
 
 if __name__ == "__main__":
     logging.info("The execution has started")
 
     try:
-        a=1/0
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e, sys)
---
+++ src/mlproject/utils.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd
+import sys 
+from src.mlproject.logger import logging
+from src.mlproject.exceptions import CustomException
+from dotenv import load_dotenv
+import pymysql
+
+load_dotenv()
+
+host = os.getenv("host")
+user = os.getenv("user")
+password = os.getenv("password")
+db = os.getenv("db")
+
+def read_sql_data():
+    logging.info("Reading data from MySQL database")
+    try:
+        mydb = pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            db=db
+        )
+        logging.info("Connection to MySQL database established successfully")
+        df=pd.read_sql_query("SELECT * FROM students", mydb)
+        print(df.head())
+
+        return df
+    except Exception as e:
+        raise CustomException(e, sys)
+    finally:
+        mydb.close()
